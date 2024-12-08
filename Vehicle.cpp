b8bd49ddad2ab0653da87eb6155eafd4c66dcabc
Kyle Baker <kyle.baker@sinclair.edu>
2024-12-08 12:20:50 -0500

    finish implementing methods but it doesn't work

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,12 +1,26 @@
-#include "Vehicle.h"
+#include VEHICLE_H
 #include <iostream>
 using namespace std;
 
 // Getters and Setters
-void Vehicle::setManufacturer(string manufacturer) {
+void Vehicle:setManufacturer(string manufacturer) {
     this->manufacturer = manufacturer;
 }
 
-string Vehicle::getManufacturer() const {
+string Vehicle:getManufacturer() const {
     return manufacturer;
+}
+
+void Vehicle::setYearBuilt(int yearBuilt) {
+    this->yearBuilt = yearBuilt;
+}
+
+int Vehicle::getYearBuilt() const {
+    return yearBuilt;
+}
+
+// display vehicle information
+void Vehicle::displayInfo() const {
+    cout << "Manufacturer: " << manufacturer << endl;
+    cout << "Year Built: " << yearBuilt << endl;
 }
