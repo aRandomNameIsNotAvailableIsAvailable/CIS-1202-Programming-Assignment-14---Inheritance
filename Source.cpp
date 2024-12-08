d3a63e8b2f76cac4d796227bdda3ff237b9fc54a
Kyle Baker <kyle.baker@sinclair.edu>
2024-12-08 00:42:11 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,64 @@
+/*
+Name: Kyle Baker
+Date: 12/7/2024
+Project Name: CIS 1202 Programming Assignment # 14 - Inheritance
+Project Description: Develop a base and derived classes to represent different types of vehicles. Then program a short driver to these classes.
+*/
+
+// libraries
+#include<cmath>
+#include<fstream>
+#include<iomanip>
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+// namespace
+using namespace std;
+
+// function prototypes
+
+void emptyFunction();
+
+// classes
+
+class emptyClass {
+private:
+	int emptyPrivateNum_01;
+	string emptyPrivateString_01;
+public:
+	int emptyPublicNum_01;
+	string emptyPublicString_01;
+};
+
+	//											+++Start Main+++
+
+int main() {
+
+	// declare variables
+
+
+
+
+
+
+
+
+	//											+++END MAIN+++
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+
+}
+
+// function definitions
+
+void emptyFunction()
+{
+	
+}
+
+// class definitions
+
